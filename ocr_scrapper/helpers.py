af1d5533a3b3b784cfc55f7f8315dc30d4685661
liya-gafurova <gali4086555@gmail.com>
2021-09-18 22:03:14 +0600

    added get areas

--- ocr_scrapper/helpers.py
+++ ocr_scrapper/helpers.py
@@ -3,8 +3,11 @@ from typing import List
 import os
 import argparse
 
+import cv2
+import pytesseract
 
 from ocr_scrapper.settings import FILES_DIR, SCREENSHOTS_DIR_NAME, TEXTS_DIR_NAME
+
 
 @dataclass
 class Recourse:
@@ -17,8 +20,10 @@ class OutDirectories:
     screenshots: str
     texts: str
 
+
 def get_filename_without_extension(get_screenshot_filename):
     return os.path.splitext(get_screenshot_filename)[0]
+
 
 def create_out_directories(recourse_name: str) -> OutDirectories:
     screenshots_dir = f'{FILES_DIR}{recourse_name}{SCREENSHOTS_DIR_NAME}'
@@ -46,3 +51,29 @@ def load_recourse_dataset(recourse_filepath: str) -> Recourse:
         links = links_file.readlines()
 
     return Recourse(name=recourse_name, links=links)
+
+
+def get_texts_from_areas(img_path: str) -> List[str]:
+    texts = []
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+    dilation = cv2.dilate(thresh1, rect_kernel, iterations=1)
+    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
+                                           cv2.CHAIN_APPROX_NONE)
+    im2 = img.copy()
+    copied_filename = 'contoured_'+get_filename_without_extension(os.path.basename(img_path)) + '.png'
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cropped = im2[y:y + h, x:x + w]
+
+        text = pytesseract.image_to_string(cropped)
+        print(f"<=={text}==>")
+        texts.append(text)
+
+
+
+    return  texts
+
--- ocr_scrapper/main.py
+++ ocr_scrapper/main.py
@@ -1,11 +1,11 @@
 import datetime
-import os
+import json
+
 from PIL import Image
 import pytesseract
 from selenium import webdriver
 
-from ocr_scrapper.helpers import get_filename_without_extension, load_recourse_dataset, create_out_directories, \
-    parse_cli_args
+from ocr_scrapper.helpers import *
 
 
 def load_webdriver():
@@ -22,16 +22,18 @@ def explore_resource(driver, resource, screenshots_directory):
 
 def recognize_screenshots(screenshots_directory, recognized_texts_directory):
     for screenshot_file in os.listdir(screenshots_directory):
-        recognized_text = pytesseract.image_to_string(Image.open(screenshots_directory + screenshot_file))
+        image_file = screenshots_directory + screenshot_file
         screenshot_name = get_filename_without_extension(screenshot_file)
-        with open(f"{recognized_texts_directory}{screenshot_name}.txt", 'w') as text_file:
-            text_file.write(recognized_text)
+        data = {'texts': get_texts_from_areas(image_file)}
+        with open(f"{recognized_texts_directory}{screenshot_name}.json", 'w') as text_file:
+            text_file.write(json.dumps(data))
 
 
 if __name__ == '__main__':
+    args = parse_cli_args()
+
     dataset = load_recourse_dataset('./links/amazon.txt')
     dirs = create_out_directories(dataset.name)
-    args = parse_cli_args()
 
     if args.mode == 'scrape':
         web_driver = load_webdriver()
