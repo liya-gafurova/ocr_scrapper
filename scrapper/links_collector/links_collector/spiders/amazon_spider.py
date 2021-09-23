741f655d025d884550cc02bd58d90cbb7d0b09cc
liya-gafurova <gali4086555@gmail.com>
2021-09-23 10:04:35 +0600

    nonworking scrapper

--- scrapper/links_collector/links_collector/spiders/amazon_spider.py
+++ scrapper/links_collector/links_collector/spiders/amazon_spider.py
@@ -2,19 +2,18 @@ import scrapy
 from scrapy.spiders import Rule
 
 
-class LinkExtractor:
-    pass
 
 
 class QuotesSpider(scrapy.Spider):
     name = "amazon"
     start_urls = [
-        "https://www.amazon.com/b?node=16225009011&pf_rd_r=9BWSJ1HB0HBXZ1NMZZJ3&pf_rd_p=5232c45b-5929-4ff0-8eae-5f67afd5c3dc&pd_rd_r=d8b98471-6bcf-4094-b8dd-4108a4d83da4&pd_rd_w=cH3eF&pd_rd_wg=vnoNK&ref_=pd_gw_unk"]
-    # rules = [Rule(LinkExtractor(), callback='parse', follow=True)]
+        'https://en.wikipedia.org/wiki/Main_Page',
+    ]
+    def __init__(self):
+        self.links = []
 
 
     def parse(self, response):
-        for link in response.xpath('//div/p/a'):
-            yield {
-                "link": self.base_url + link.xpath('.//@href')
-            }
+        self.links.append(response.url)
+        for href in response.css('a::attr(href)'):
+            yield response.follow(href, self.parse)
