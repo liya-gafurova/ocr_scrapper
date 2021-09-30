068e1824f2684655bf257446f5340e095d67c055
liya-gafurova <gali4086555@gmail.com>
2021-09-30 10:00:08 +0600

    WIP added links analysis

--- ocr_scrapper/selectorlib_scraper/links_filter.py
+++ ocr_scrapper/selectorlib_scraper/links_filter.py
@@ -1,5 +1,15 @@
+import re
 from typing import List
+
+bestsellers_pattern = re.compile('/gp/bestsellers')
 
 
 def filter_unique_links(links: List[str]):
     return list(set(links))
+
+
+def filter_best_sellers_urls(link):
+    found_group = re.search(bestsellers_pattern, link)
+    return False if found_group is None else True
+
+
