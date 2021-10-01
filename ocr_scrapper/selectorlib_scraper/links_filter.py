8b4656eae3b1064e5a907280abd92fe4af5c023a
liya-gafurova <gali4086555@gmail.com>
2021-10-01 10:22:46 +0600

    WIP added links analysis

--- ocr_scrapper/selectorlib_scraper/links_filter.py
+++ ocr_scrapper/selectorlib_scraper/links_filter.py
@@ -1,15 +1,22 @@
 import re
 from typing import List
 
-bestsellers_pattern = re.compile('/gp/bestsellers')
+customer_reviews = re.compile('#customerReviews')  # Not
+offering_list = re.compile('/gp/offer-listing')  # Not
+
+NOT_patterns = [customer_reviews, offering_list]
 
 
 def filter_unique_links(links: List[str]):
     return list(set(links))
 
 
-def filter_best_sellers_urls(link):
-    found_group = re.search(bestsellers_pattern, link)
-    return False if found_group is None else True
+def filter_url_by_pattern(pattern, link, should_not_contain=False):
+    found_group = re.search(pattern, link)
+    return found_group is None and should_not_contain
 
 
+def filters_links(links_list: List[str]) -> List[str]:
+    unique_links = filter_unique_links(links_list)
+    filtered_links = [link for link in unique_links if filter_unique_links(link, NOT_patterns)]
+    return filtered_links
