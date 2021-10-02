00a293e9824b91f99da5d63d6c84d34abe48b5f7
liya-gafurova <gali4086555@gmail.com>
2021-10-02 11:12:08 +0600

    WIP added links analysis

--- ocr_scrapper/selectorlib_scraper/links_filter.py
+++ ocr_scrapper/selectorlib_scraper/links_filter.py
@@ -1,3 +1,4 @@
+import json
 import re
 from typing import List
 
@@ -7,16 +8,41 @@ offering_list = re.compile('/gp/offer-listing')  # Not
 NOT_patterns = [customer_reviews, offering_list]
 
 
+# функициональное программирование? - map/reduce - изучить
+
 def filter_unique_links(links: List[str]):
     return list(set(links))
 
 
-def filter_url_by_pattern(pattern, link, should_not_contain=False):
-    found_group = re.search(pattern, link)
-    return found_group is None and should_not_contain
+def filter_url_by_pattern(pattern, link, should_contain):
+    found_pattern = True if re.search(pattern, link) else False
+    return found_pattern == should_contain
 
 
-def filters_links(links_list: List[str]) -> List[str]:
+def filter_url_by_all_patterns(patterns, link, should_contain=False):
+    ulr_not_containing_bad_patterns: List[bool] = [filter_url_by_pattern(pattern, link, should_contain)
+                                                   for pattern in patterns]
+    return all(ulr_not_containing_bad_patterns)
+
+
+def filter_links(links_list: List[str]) -> List[str]:
     unique_links = filter_unique_links(links_list)
-    filtered_links = [link for link in unique_links if filter_unique_links(link, NOT_patterns)]
+    filtered_links = [link for link in unique_links if
+                      filter_url_by_all_patterns(NOT_patterns, link, should_contain=False)]
     return filtered_links
+
+
+with open('output.jsonl', 'r') as links_data, open('filtered_links.json', 'w') as outfile2:
+    json_data = json.loads(links_data.read())
+    links = json_data.get('link')
+    print(links)
+
+    filtered = filter_links(links)
+    print(filtered)
+    outfile2.write(
+        json.dumps(
+            {
+                'link': filtered
+            }
+        )
+    )
